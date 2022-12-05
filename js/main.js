bc0d0cbc413588df6d93a51bd0b81a180c22aeba
diskrize <ignatenko.d.1990@gmil.com>
2022-12-05 21:02:04 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '60px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+})
+var acc = document.getElementsByClassName("accordion");
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
